06c7db37429bad52ab862a6f3abc88feaa55602c
eamorenor <104174995+eamorenor@users.noreply.github.com>
2022-10-18 23:01:16 -0500

    Sprint

--- sprint1.dart
+++ sprint1.dart
@@ -1,7 +1,75 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print('Hello, World!');
-  
-    a==2 ||a==3 ? print (' es bajo'):
-       a==4?print ('es alto'):
-            print ('ganaste'); 
+  runApp(const TurisBogota());
+}
+
+class TurisBogota extends StatelessWidget {
+  const TurisBogota({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
